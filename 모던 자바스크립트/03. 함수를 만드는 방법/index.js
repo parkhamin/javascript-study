c239555fef8186be496d9cd958f644afbe2ae546
parkhamin <parkhamint@naver.com>
2026-05-29 17:58:52 +0900

    함수를 만드는 방법

---
+++ 모던 자바스크립트/03. 함수를 만드는 방법/index.js
@@ -0,0 +1,14 @@
+// 함수 표현식 (Function Expression)
+const printCodeit = function () {
+  console.log('Codeit');
+};
+
+printCodeit();
+
+// 함수 선언 방식
+function print() {
+  console.log('Codeit');
+}
+
+// 함수 선언 방식은 선언 전에 호출 가능(보편적)
+// 함수 표현 방식은 선언 전에 호출 불가능
